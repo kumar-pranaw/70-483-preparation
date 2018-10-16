a258d7ea6e21411e47d3e3b74ffb7116f1aee598
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-10-16 07:52:34 +0530

    Added Class thread 1) Checking multiple threads without any delay 2)Checking with delay

--- 70-483 Prep/Console App 1/Program.cs
+++ 70-483 Prep/Console App 1/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace Console_App_1
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**************************** THREADING ****************************");
+            Console.WriteLine("\n\n");
+            //Thread.AnotherThread();
+            Thread.BackGroundThreads();
         }
     }
 }
---
+++ 70-483 Prep/Console App 1/Thread.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public static class Thread
+    {
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Threadproc: {0}", i);
+                System.Threading.Thread.Sleep(0);
+            }
+        }
+        public static void AnotherThread()
+        {
+            System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadMethod));
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread: Do Some work. ");
+                System.Threading.Thread.Sleep(0);
+            }
+            t.Join();
+        }
+
+        public static void ThreadMethod2()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProcess: {0}", i);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public static void BackGroundThreads()
+        {
+            System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadMethod2));
+            t.IsBackground = false;// Changin isBackground to true will not reflect the changes application will quit
+            t.Start();
+        }
+
+        /// <summary>
+        /// This method takes an argument which will be passing when thread will be started
+        /// </summary>
+        /// <param object="o"></param>
+        public static void ThreadMethodWithParam(object o)
+        {
+            for(int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i)
+;            }
+        }
+    }
+}
